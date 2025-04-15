6b170c13fa0e430cde3eda3cb6305c6adab0be56
Mohamed Shahin <ms4211913@gmail.com>
2025-04-15 12:07:22 +0200

    initial_commit

---
+++ 02 DeleteDebugger/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace RemoveDebugger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Teal;
+            this.button1.Location = new System.Drawing.Point(191, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Delete Debugger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(546, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Welcome , You can delete Registry \"Debuger\" value here\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(139, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Just Press The Button Below";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.SeaGreen;
+            this.progressBar1.Location = new System.Drawing.Point(22, 184);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(513, 23);
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(562, 219);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Remove Debugger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.button1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (progressValue < 100)
+            {
+                progressValue++;
+                progressBar1.Value = progressValue;
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Process Completed!");
+            }
+        }
+        #endregion
+        private System.Windows.Forms.Button button1;
+
+        // debugger path
+        private static string DebuggerPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options";
+        private Label label1;
+        private Label label2;
+        private Timer timer1;
+        private int progressValue = 0;
+        private ProgressBar progressBar1;
+    }
+}
+
---
+++ 02 DeleteDebugger/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveDebugger
+{
+    public partial class Form1 : Form
+    {
+        private RegistryKey ImageFileExecutionOptionsKey = Registry.LocalMachine.OpenSubKey(DebuggerPath);
+
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = ImageFileExecutionOptionsKey.SubKeyCount;
+            progressBar1.Step = 1;
+        }
+        private void Button1_MouseHover()
+        {
+            this.button1.ForeColor = System.Drawing.Color.MintCream;
+            this.button1.BackColor = System.Drawing.Color.Teal;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Button1_MouseHover();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+            progressBar1.Value = 0; // Reset progress
+
+            try
+            {
+                
+                if (ImageFileExecutionOptionsKey != null)
+                {
+                    foreach(string subKey in ImageFileExecutionOptionsKey.GetSubKeyNames())
+                    {
+                        if(subKey.Equals("MsSense.exe"))
+                            continue;
+                        else {
+                            RegistryKey key = Registry.LocalMachine.OpenSubKey($"{DebuggerPath}\\{subKey}", true);
+                            if (key.GetValue("Debugger") != null)
+                            {
+                                key.DeleteValue("Debugger");
+                            }
+
+                            await Task.Delay(50); // Simulate a delay for each iteration
+
+                            // Update progress bar
+                            progressBar1.Value++;
+                        }
+                        
+                    }
+                    MessageBox.Show("Process Completed!");
+
+                }
+                
+
+            }
+            catch (Exception ex) { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
